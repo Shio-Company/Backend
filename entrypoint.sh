30b306dec5164d18ef91629ff9ab8975cbea032a
Henrique Camelo Quenino <hcquenino@gmail.com>
2026-04-29 11:08:12 -0300

    feat: add railway deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e
+
 python manage.py migrate --noinput
 
-python manage.py runserver 0.0.0.0:8000
+if [ "$DEBUG" = "True" ] || [ "$DEBUG" = "true" ] || [ "$DEBUG" = "1" ]; then
+    exec python manage.py runserver 0.0.0.0:${PORT:-8000}
+fi
+
+python manage.py collectstatic --noinput
+exec gunicorn core.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3
